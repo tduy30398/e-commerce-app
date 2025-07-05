3c9032022b4ae10ba845d416d335d99b4038b82f
Your Name <you@example.com>
2025-07-05 23:50:31 +0700

    header & shadcn

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link';
+import CustomButton from '@/components/molecules/CustomButton';
+import Image from 'next/image';
 
 export default function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen text-center p-4">
-            <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-            <p className="mb-6 text-lg text-gray-600">
-                Sorry, the page you are looking for does not exist.
+        <div className="flex flex-col items-center h-screen text-center p-4 justify-center">
+            <h1 className="text-5xl font-black mb-4">Oops!</h1>
+            <p className="mb-6 text-lg font-medium text-gray-600">
+                Something went wrong.
             </p>
-            <Link href="/" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-                Go back home
-            </Link>
+            <Image src={'/images/404.png'} alt="404" width={655} height={290} />
+            <CustomButton linkProps={{ href: '/', label: 'Go home' }} className='mt-8' />
         </div>
     );
 }
