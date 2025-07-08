40e77038b3d33749f4961e0466222bf928515d37
Thanh Duy <duy.lt@3forcom.com>
2025-07-08 16:03:04 +0700

    fix pre-render

--- client/app/(root)/page.tsx
+++ client/app/(root)/page.tsx
@@ -1,13 +1,11 @@
 import PartnerCarousel from '@/components/templates/PartnerCarousel';
 import HomeBanner from '@/components/templates/HomeBanner';
-import NewArrivals from '@/components/templates/NewArrivals';
 
 const Home = () => {
     return (
         <div>
             <HomeBanner />
             <PartnerCarousel />
-            <NewArrivals />
         </div>
     );
 };
