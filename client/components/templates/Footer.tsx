4fba770ee7e9c9c2793d99b2a363cbbe17722e0b
Your Name <you@example.com>
2025-07-11 08:08:37 +0700

    footer

---
+++ client/components/templates/Footer.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable @next/next/no-img-element */
+import { footerDummy, paymentMethods } from '@/public/dummy/general'
+import { Facebook, Github, Instagram, Twitter } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Footer = () => {
+    return (
+        <footer className='pt-20 bg-flash-white'>
+            <div className="section-container flex items-start gap-20 pb-[50px] border-b-[1px] border-gray-300">
+                <div className="w-[calc((3/12)*100%)] mt-[6px]">
+                    <Image
+                        src='/icons/main-logo.svg'
+                        alt='logo'
+                        width={142}
+                        height={22}
+                        className='w-[126px] h-[18px] sm:w-[142px] sm:h-[22px]'
+                    />
+                    <p className='mt-6 text-base'>We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
+                    <div className="flex items-center mt-9 gap-2">
+                        <Link
+                            href='https://twitter.com'
+                            target='_blank'
+                            className="p-2 rounded-full border bg-flash-white border-gray-500"
+                        >
+                            <Twitter className='size-5' />
+                        </Link >
+                        <Link
+                            href='https://facebook.com'
+                            target='_blank'
+                            className="p-2 rounded-full border bg-flash-white border-gray-500"
+                        >
+                            <Facebook className='size-5' />
+                        </Link >
+                        <Link
+                            href='https://instagram.com'
+                            target='_blank'
+                            className="p-2 rounded-full border bg-flash-white border-gray-500"
+                        >
+                            <Instagram className='size-5' />
+                        </Link >
+                        <Link
+                            href='https://github.com'
+                            target='_blank'
+                            className="p-2 rounded-full border bg-flash-white border-gray-500"
+                        >
+                            <Github className='size-5' />
+                        </Link >
+                    </div>
+                </div>
+                <div className="flex items-start w-full">
+                    {footerDummy.map((item, index) => (
+                        <div key={index} className="w-1/4">
+                            <h3 className='text-xl font-semibold uppercase'>{item.title}</h3>
+                            <ul >
+                                {item.links.map((link, index) => (
+                                    <li key={index} className='mt-4'>
+                                        <Link
+                                            href={link.link}
+                                            className='text-base text-gray-800 hover:underline'
+                                        >
+                                            {link.title}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="pb-20 mt-4 section-container flex items-center justify-between">
+                <p>
+                    Shop.co © 2000-2023, All Rights Reserved
+                </p>
+                <div className="flex items-center gap-3">
+                    {paymentMethods.map((item, index) => (
+                        <div key={index} className="px-4 bg-white rounded-[10px] h-9 flex items-center">
+                            <img src={item.logo} alt={item.name} width={34} height={15} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- client/public/dummy/general.ts
+++ client/public/dummy/general.ts
@@ -45,3 +45,114 @@ export const partnerLogos = [
     { name: "Prada", logo: '/images/partners/prada.png' },
     { name: "Calvin", logo: '/images/partners/calvin.png' },
 ];
+
+export const footerDummy = [
+    {
+        title: 'Company',
+        links: [
+            {
+                title: 'About',
+                link: '/about'
+            },
+            {
+                title: 'Features',
+                link: '/features'
+            },
+            {
+                title: 'Works',
+                link: '/works'
+            },
+            {
+                title: 'Careers',
+                link: '/careers'
+            },
+        ]
+    },
+    {
+        title: 'Help',
+        links: [
+            {
+                title: 'Customer Support',
+                link: '/customer-support'
+            },
+            {
+                title: 'Delivery Details',
+                link: '/delivery-details'
+            },
+            {
+                title: 'Terms & Conditions',
+                link: '/terms-conditions'
+            },
+            {
+                title: 'Privacy Policy',
+                link: '/privacy-policy'
+            },
+        ]
+    },
+    {
+        title: 'FAQs',
+        links: [
+            {
+                title: 'Account',
+                link: '/account'
+            },
+            {
+                title: 'Payment',
+                link: '/payment'
+            },
+            {
+                title: 'Order',
+                link: '/order'
+            },
+            {
+                title: 'Shipping',
+                link: '/shipping'
+            },
+        ]
+    },
+    {
+        title: 'Resources',
+        links: [
+            {
+                title: 'Free eBooks',
+                link: '/free-ebooks'
+            },
+            {
+                title: 'Development Tutorial',
+                link: '/development-tutorial'
+            },
+            {
+                title: 'How to - Blog',
+                link: '/how-to-blog'
+            },
+            {
+                title: 'Youtube Playlist',
+                link: '/youtube-playlist'
+            },
+        ]
+    },
+]
+
+export const paymentMethods = [
+    {
+        name: 'Apple',
+        logo: '/icons/payment/applePay.svg'
+    },
+    {
+        name: 'Visa',
+        logo: '/icons/payment/visa.svg'
+    },
+
+    {
+        name: 'Mastercard',
+        logo: '/icons/payment/mastercard.svg'
+    },
+    {
+        name: 'Paypal',
+        logo: '/icons/payment/paypal.svg'
+    },
+    {
+        name: 'Google',
+        logo: '/icons/payment/googlePay.svg'
+    }
+]
