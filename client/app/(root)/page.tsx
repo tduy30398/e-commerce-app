70c9cffc67a84c6996572d9d29f0d74ab0ba0da4
Your Name <you@example.com>
2025-07-27 09:04:14 +0700

    carousel-shadcn

--- client/app/(root)/page.tsx
+++ client/app/(root)/page.tsx
@@ -1,6 +1,6 @@
-import PartnerCarousel from '@/components/templates/PartnerCarousel';
 import HomeBanner from '@/components/templates/HomeBanner';
 import NewArrivals from '@/components/templates/NewArrivals';
+import PartnerCarousel from '@/components/templates/PartnerCarousel';
 
 const Home = () => {
   return (
--- client/components/templates/PartnerCarousel.tsx
+++ client/components/templates/PartnerCarousel.tsx
@@ -1,41 +1,37 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
 import React from 'react';
-import Slider from 'react-slick';
+import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel';
 import { partnerLogos } from '@/public/dummy/general';
+import Autoplay from 'embla-carousel-autoplay';
 
-const PartnerCarousel: React.FC = () => {
-  const settings = {
-    arrows: false,
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 10,
-    speed: 8000,
-    cssEase: 'linear',
-    pauseOnHover: false,
-    responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 4 } },
-      { breakpoint: 768, settings: { slidesToShow: 3 } },
-    ],
-  };
-
+const PartnerCarousel = () => {
   return (
     <section className="bg-black h-20 sm:h-[122px] py-6 sm:py-10">
-      <Slider {...settings}>
-        {partnerLogos.map((partner, index) => (
-          <div key={index}>
-            <div className="flex justify-center items-center max-sm:mx-4">
-              <img src={partner.logo} alt={partner.name} />
-            </div>
-          </div>
-        ))}
-      </Slider>
+      <Carousel
+        opts={{
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 2000,
+            stopOnInteraction: false,
+          }),
+        ]}
+      >
+        <CarouselContent>
+          {partnerLogos.map((partner, index) => (
+            <CarouselItem key={index} className="basis-1/4">
+              <img
+                src={partner.logo}
+                alt={partner.name}
+                className="max-h-12 object-contain select-none mx-auto"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </section>
   );
 };
