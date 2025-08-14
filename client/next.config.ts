0a3788b335adeb00c9c895cdd5b835de3bc9f3d6
Your Name <you@example.com>
2025-08-14 22:16:59 +0700

    next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://res.cloudinary.com/**')],
-    domains: ['lh3.googleusercontent.com', 'platform-lookaside.fbsbx.com'],
+    domains: [
+      'lh3.googleusercontent.com',
+      'platform-lookaside.fbsbx.com',
+      'avatars.githubusercontent.com',
+    ],
   },
   devIndicators: false,
 };
