f7d8d6f24024eb2489758abe4ccfb25b9d9d00ac
Thanh Duy <duy.lt@3forcom.com>
2025-08-14 13:20:49 +0700

    oauth login

---
+++ client/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+})
