bff468485658da67415361c3fa4aa2aeff402642
Thanh Duy <duy.lt@3forcom.com>
2025-09-15 14:21:38 +0700

    implement redirect

--- client/app/[locale]/admin/product/page.tsx
+++ client/app/[locale]/admin/product/page.tsx
@@ -1,12 +1,31 @@
+import { auth } from '@/auth';
 import TableSkeleton from '@/components/molecules/TableSkeleton';
 import ProductTable from '@/components/templates/ProductTable';
+import { redirect } from '@/i18n/navigation';
+import { ROUTES } from '@/lib/constants';
+import { getLocale } from 'next-intl/server';
+import { cookies } from 'next/headers';
 import { Suspense } from 'react';
 
 export const metadata = {
   title: 'Product Dashboard',
 };
 
-const ProductDashboard = () => {
+interface CookiesTypes {
+  name?: string;
+  value?: string;
+}
+
+const ProductDashboard = async () => {
+  const session = await auth();
+  const locale = await getLocale();
+  const cookieStore = await cookies();
+  const refreshToken: CookiesTypes | undefined = cookieStore.get('refreshToken');
+
+  if (!session && !refreshToken) {
+    redirect({ href: ROUTES.LOGIN, locale });
+  }
+
   return (
     <Suspense fallback={<TableSkeleton />}>
       <ProductTable />
